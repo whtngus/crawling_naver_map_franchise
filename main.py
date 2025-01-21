470ce11d21baa912d4810346a1fdb30fa9b39eb1
sh <whtngus3232@naver.com>
2025-01-21 09:56:37 +0900

    매일 재실행 되도록 코드 변경

--- main.py
+++ main.py
@@ -6,7 +6,9 @@
 #
 
 from collect.kakao_api_m import KakaoAPIManager
+from time import time
 
-
-kakao_api = KakaoAPIManager(progress_file_path='./save_data/')
-kakao_api.collect_stores_in_parallel(max_workers=10)
+while True:
+    kakao_api = KakaoAPIManager(progress_file_path='./save_data/')
+    kakao_api.collect_stores_in_parallel(max_workers=10)
+    time.sleep(3600*23)
